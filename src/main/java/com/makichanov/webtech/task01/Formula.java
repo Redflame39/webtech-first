20a998ce55f61c56d4502112b4405e2ba2dc227b
Redflame39 <vanya133719@gmail.com>
2021-09-25 09:55:44 +0300

    task01

---
+++ src/main/java/com/makichanov/webtech/task01/Formula.java
@@ -0,0 +1,9 @@
+package com.makichanov.webtech.task01;
+
+public class Formula {
+    public double calculateFormula(double x, double y) {
+        double numeratorVal = 1 + Math.sin(x + y);
+        double denominatorVal = 2 + Math.abs(x - (2 * x) / (1 + Math.pow(x,2) * Math.pow(y, 2)));
+        return numeratorVal / denominatorVal + x;
+    }
+}
